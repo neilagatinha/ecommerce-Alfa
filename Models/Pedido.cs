f175cba98eaa63be338fbefb628f55836fd0c47c
Neila <neila48377036@edu.pa.senac.br>
2023-09-12 16:44:20 -0300

    atualizações de cod II

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,13 +1,16 @@
-﻿namespace ecommerce_db.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ecommerce_db.Models
 {
     public class Pedido
     {
         public int id { get; set; }
+        public int IdCliente { get; set; }
         public  DateTime data  { get; set; }
         public float valorTotal { get; set; }
         public endereco? endereco { get; set; }
-        public Clientes Clientes { get; set; }
         public ICollection<ItemPedido> ItensdoPedido { get; set; } = new List<ItemPedido>();
-
+        [ForeignKey("IdCliente")] 
+        public Clientes Clientes { get; set; }
     }
 }
