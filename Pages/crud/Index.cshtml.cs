17e464fccc5c5eb3d4a141aea45ccf0e2f975822
Lucas Campos <lucas10989316@edu.pa.senac.br>
2023-09-18 17:49:02 -0300

    Código crud index

---
+++ Pages/crud/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ecommerce_db.Pages.crud
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
