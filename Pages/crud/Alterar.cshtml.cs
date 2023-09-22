412bef31d4fb6a3d8a6ae5873441efdac2419db1
Administrador <Administrador@CST30LAB202D004>
2023-09-22 17:47:40 -0300

    Alterações

--- Pages/crud/Alterar.cshtml.cs
+++ Pages/crud/Alterar.cshtml.cs
@@ -2,35 +2,30 @@ using ecommerce_db.Data;
 using ecommerce_db.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce_db.Pages.crud
 {
-    public class IncluirModel : PageModel
-    {
+    public class AlterarModel : PageModel {
         private readonly AppDbContext _context;
 
-        public IncluirModel(AppDbContext context)
+        public AlterarModel(AppDbContext context)
         {
             _context = context;
         }
 
-        public void OnGet()
-        {
-           
-        }
-
         [BindProperty]
         public Clientes clientes { get; set; }
+        public async Task<IActionResult> OnGet(int id)
+        {
+            clientes = await _context.cliente.FirstOrDefaultAsync(c => c.Id == id);
+
+            return Page();
+        }
 
         public async Task<IActionResult> OnPostAsync() {
-            try{
-                _context.cliente.Add(clientes);
-                await _context.SaveChangesAsync();
-            
-            }catch{
-                Debug.WriteLine(clientes.Name);
-            }
+            _context.Attach(clientes).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
 
             return RedirectToPage("./Listar");
         }
