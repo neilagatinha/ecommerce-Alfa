805d9be8c1ffa7ed88b416401c579428d7731189
Neila <neila48377036@edu.pa.senac.br>
2023-09-26 17:58:14 -0300

    data notacion pedido

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ecommerce_db.Models
 {
     public class Pedido
     {
+        [Key]
         public int id { get; set; }
         public int IdCliente { get; set; }
         public  DateTime data  { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "O campo {0} deve ter uma data válida")]
         public float valorTotal { get; set; }
         public endereco? endereco { get; set; }
       
