3c47c3e0505c16b6021362a409c2e8decc23c063
Neila <neila48377036@edu.pa.senac.br>
2023-09-26 17:56:59 -0300

    criação de novas branchs- com Data Notacion endereço e pedido

--- Models/endereco.cs
+++ Models/endereco.cs
@@ -1,8 +1,14 @@
-﻿namespace ecommerce_db.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_db.Models
 {
-    public class endereco{
-        public int id {  get; set; }
+    public class endereco {
+        [Key]
+        public int id { get; set; }
         public string? cep { get; set; }
+
+        [Required ]
+        [MaxLength(11, ErrorMessage = "O campo {0} deve ter tamanho igual a {8}")]
         public string? cidade { get; set; }
         public string? bairro { get; set; }
         public string? lougadouro { get; set; }
