867aa1497734ea7dc09d1132147f2f1a5cd68e5f
Neila <neila48377036@edu.pa.senac.br>
2023-09-27 15:18:07 -0300

    data notacion

--- Models/endereco.cs
+++ Models/endereco.cs
@@ -5,13 +5,22 @@ namespace ecommerce_db.Models
     public class endereco {
         [Key]
         public int id { get; set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
+        [MaxLength(8, ErrorMessage = "O campo {0} deve ter tamanho igual a 8")]
         public string? cep { get; set; }
 
-        [Required ]
-        [MaxLength(11, ErrorMessage = "O campo {0} deve ter tamanho igual a {8}")]
+        [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
         public string? cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
         public string? bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
         public string? lougadouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
         public string? uf { get; set; }
 
     }
