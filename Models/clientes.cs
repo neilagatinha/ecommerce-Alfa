cae74856a749535e9c4d02371f912c583a01f9e2
Neila <neila48377036@edu.pa.senac.br>
2023-09-27 18:01:22 -0300

    código de alterae e incluir

--- Models/clientes.cs
+++ Models/clientes.cs
@@ -6,13 +6,15 @@ namespace ecommerce_db.Models
     {
         [Key]
         public int Id { get; set; }
-        [MaxLength(11, ErrorMessage = "O campo {Name} deve ter tamanho igual a {100}")]
-        [MinLength(11, ErrorMessage = "O campo {Name} deve ter no mínimo {15} caracteres.")]
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
         public string? Name { get; set; }
 
-        [EmailAddress(ErrorMessage = "O campo {Nome} deve conter um endereço de e-mail válido")]
+        [EmailAddress(ErrorMessage = "O campo {0} deve conter um endereço de e-mail válido")]
         public string? email  { get; set; }
-        [MaxLength(11, ErrorMessage = "O campo {telefone} deve ter no mínimo {9} caracteres.")]
+
+        [MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
         public int telefone  { get; set; }
 
         public endereco? endereco { get; set; }
