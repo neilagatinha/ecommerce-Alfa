81c365c720f7effcdfd212f61560b759416110fc
Lucas Campos <lucas10989316@edu.pa.senac.br>
2023-10-03 17:51:39 -0300

    tela crud UnidadeDeMedida

---
+++ Models/Unidade-de-Medida.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_db.Models
+{
+    public class UnidadeDeMedida
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20, ErrorMessage = "O campo {0} deve ter tamanho igual {1}")]
+        [MinLength(2, ErrorMessage = "O campo {0} deve ter tamanho igual {1}")]
+        public string Nome { get; set; }
+        [MaxLength(3, ErrorMessage = "O campo {0} deve ter tamanho igual {1}")]
+        [MinLength(2, ErrorMessage = "O campo {0} deve ter tamanho igual {1}")]
+        public string Sigla { get; set;}
+
+    }
+}
