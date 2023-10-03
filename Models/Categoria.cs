22d4caf51e29e55b9152c37d17861873b98b3e19
Administrador <Administrador@CST30LAB202D004>
2023-10-03 17:52:26 -0300

    Criação da classe Categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_db.Models
+{
+    public class Categoria {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MinLength(2, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+        public string? Nome { get; set; }
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MinLength(2, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+        public string? Descricao { get; set; }
+    }
+}
