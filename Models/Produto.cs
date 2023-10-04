fcdfac6000a6e7c31671e2b5faf23e1e454d9e3f
Neila <neila48377036@edu.pa.senac.br>
2023-10-04 17:18:08 -0300

    design

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,14 +8,14 @@ namespace ecommerce_db.Models
             public int Id { get; set; }
             [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
             [MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
-            public string? nome { get; set; }
+            public string nome { get; set; }
 
             [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
             [MinLength(1, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
-            public string? descricao { get; set; }
+            public string descricao { get; set; }
 
-            public string? categoria { get; set; }
-            public float? preco { get; set; }
+            public string categoria { get; set; }
+            public float preco { get; set; }
             public int unidadedeMedida { get; set; }
             public ICollection<ItemPedido> ItensdoPedido { get; set; } = new List<ItemPedido>();
     }
