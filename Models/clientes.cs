647426a51b910ed971e45c44417be65818a8b4ea
Neila <neila48377036@edu.pa.senac.br>
2023-10-06 16:17:57 -0300

    Modificações no cliente e endereço

--- Models/clientes.cs
+++ Models/clientes.cs
@@ -13,11 +13,17 @@ namespace ecommerce_db.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get; set; }
 
+		[MaxLength(11, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+		[MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
+        public string CPF { get; set; }
+
         [EmailAddress(ErrorMessage = "O campo {0} deve conter um endereço de e-mail válido")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string email  { get; set; }
 
- 	    [MaxLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+ 	    [MaxLength(20, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        [MinLength(11,ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres ")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string telefone { get; set; }
 
--- Models/endereco.cs
+++ Models/endereco.cs
@@ -4,24 +4,31 @@ namespace ecommerce_db.Models
 {
     public class endereco {
         [Key]
-        public int id { get; set; }
+        public int Id { get; set; }
 
 
         [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
         [MaxLength(8, ErrorMessage = "O campo {0} deve ter tamanho igual a 8")]
-        public string? cep { get; set; }
+        public string CEP { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
-        public string? cidade { get; set; }
+        public string Cidade { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
-        public string? bairro { get; set; }
+        public string Bairro { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
-        public string? lougadouro { get; set; }
+        public string Lougadouro { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é de campo obrigatório.")]
-        public string? uf { get; set; }
+        [MaxLength(2,ErrorMessage = "O campo {0} deve ter tamanho no maximo a 2 caracter")]
+        public string UF { get; set; }
+
+        [MaxLength(50,ErrorMessage ="O campo {0} só pode ter {1}")]
+        public string Referencia { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é campo obrigatório")]
+        public string Numero { get; set; }
 
     }
 }
