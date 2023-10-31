48020651a2c2246b5035c2fc7e50f7443bfb75b3
Neila <neila48377036@edu.pa.senac.br>
2023-10-31 18:01:31 -0300

    exemplo

---
+++ Pages/exemplo 2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ecommerce_db.Pages
+{
+    public class exemplo_2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
